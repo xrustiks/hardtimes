b2c21b2dfcda45d64ae05f70e911b0ed9a08c901
xrustik666 <kilorust@gmail.com>
2024-10-13 21:01:57 +0200

    getFavorites function completed

--- server/routes/getFavorites.js
+++ server/routes/getFavorites.js
@@ -1,9 +1,20 @@
+import openConnection from '../db/connection.js';
+
 const getFavorites = async(req, res) => {
+  let connection;
   try {
-    return res.status(200).json({ message: `Your favorite quotes are all here` });
+    connection = await openConnection();
+    await connection.query('USE quote');
+    const [favoriteQuotes] = await connection.query(`SELECT * FROM quote WHERE isFavorite = 1`);
+
+    return res.status(200).json({ favoriteQuotes });
   } catch(error) {
     console.error(error);
     return res.status(500).json({ message: `Failed retrieving favorites` });
+  } finally {
+    if (connection) {
+      await connection.end();
+    }
   }
 }
 
