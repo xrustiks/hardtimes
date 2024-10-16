d26f9d728575f38aad0756f5933da8f36882a885
xrustik666 <kilorust@gmail.com>
2024-10-16 23:25:27 +0200

    verifyUser function is better commented out

--- server/utils/verifyUser.js
+++ server/utils/verifyUser.js
@@ -1,24 +1,21 @@
 import { compare } from 'bcrypt';
 
 const verifyUser = async(connection, email, password) => {
-  // Searching for a user by email
+  // Extracting the password from db for the given email
   const query = 'SELECT password FROM users WHERE email = ?';
-  // Receiving the array that contains the object with user data
-  const [result] = await connection.query(query, [email]);
+  // Receiving the array that contains the object with user data 
+  // (connection.query contains array of two arrays: user data and metadata)
+  const [userData] = await connection.query(query, [email]);
 
   // The first check is to see if the user with the given email exists
-  if (result.length === 0) {
+  if (userData.length === 0) {
     return { success: false, message: 'Invalid email or user doesn\'t exist' };
   }
 
-  // Extracting the hashed password from user object
-  const hashedPassword = result[0].password;
+  // Extracting the password from user object
+  const hashedPassword = userData[0].password;
   // Compare the hashed password in database with the one that sent by user
   const isPasswordCorrect = await compare(password, hashedPassword);
-
-  console.log('Hashed password from DB:', hashedPassword);
-  console.log('Password provided by user:', password);
-  console.log('Comparison result:', isPasswordCorrect);
 
   // The second check is to see if the password is correct
   if (!isPasswordCorrect) {
