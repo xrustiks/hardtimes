d86db7260a5e4c83d08006a38e9e97417d6bc855
xrustik <kilorust@gmail.com>
2024-10-22 11:02:10 +0200

    authenticate refactored a little bit

--- server/auth/authenticate.js
+++ server/auth/authenticate.js
@@ -3,7 +3,10 @@ import verifyToken from './verifyToken.js';
 // Middleware for verifying JWT token
 export const authenticate = (req, res, next) => {
   // Getting token out of the request headers
-  const token = req.headers['authorization']?.split(' ')[1];
+  let token;
+  if (req.headers['authorization']) {
+    token = req.headers['authorization'].split(' ')[1];
+  }
   // console.log('Token:', token);
 
   if (!token) {
