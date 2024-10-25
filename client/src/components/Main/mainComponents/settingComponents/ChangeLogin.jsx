299fb8689445131f102491308e0dcbb21ea4f271
xrustik <kilorust@gmail.com>
2024-10-25 13:53:06 +0200

    ChangeLogin component refactored but not finished

--- client/src/components/Main/mainComponents/settingComponents/ChangeLogin.jsx
+++ client/src/components/Main/mainComponents/settingComponents/ChangeLogin.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 const ChangeLogin = () => {
-  const newLogin = document.getElementById('new-login').value;
+  const [ message, setMessage ] = useState('');
 
   const handleSubmit = async(e) => {
     e.preventDefault();
@@ -11,28 +11,28 @@ const ChangeLogin = () => {
       headers: {
         'Content-Type': 'application'
       },
-      body: JSON.stringify({
-        newLogin
-      })
+      body: JSON.stringify({ newLogin })
     });
 
+    
     if (response.ok) {
       const result = await response.json();
-      console.log(result);
+      setMessage(result.message);
     }
   }
 
   return (
     <div className="change-login">
-      <form className="change-login-form">
+      <form className="change-login-form" onSubmit={ handleSubmit }>
         <label htmlFor="new-login">New login:</label>
         <input type="text" id="new-login" name="new-login" required />
 
         <label htmlFor="new-login-confirm">Confirm new login:</label>
         <input type="text" id="new-login-confirm" name="new-login-confirm" required />
 
-        <button type="submit" onSubmit={ handleSubmit }>Change login</button>
+        <button type="submit">Change login</button>
       </form>
+      { message && <p>{ message }</p> }
     </div>
   );
 }
