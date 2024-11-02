feb3f98832afec23703148a20a5803e337b49c50
xrustik <kilorust@gmail.com>
2024-11-02 03:16:39 +0100

    favorites link appears only for logged in users

--- client/src/components/Header/headerComponents/NavigationBar.jsx
+++ client/src/components/Header/headerComponents/NavigationBar.jsx
@@ -1,13 +1,20 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import './NavigationBar.css';
 
 const NavigationBar = () => {
+  const [isLogin, setIsLogin] = useState(false);
+
+  useEffect(() => {
+    localStorage.getItem('token') ? setIsLogin(true) : setIsLogin(false);
+  }, []);
+
   return (
     <div className="nav-bar">
       <Link to="/">Главная</Link>
       <Link to="/post-quote">Добавить цитату</Link>
-      <Link to="/favorites">Избранные цитаты</Link>
+      {isLogin && <Link to="/favorites">Избранные цитаты</Link>}
     </div>
   )
 }
