56bf347f9711f759b2d8f8f006fc59c9434fb00b
xrustik666 <kilorust@gmail.com>
2024-11-12 23:23:03 +0100

    category selection works but menu is inside home component

--- server/helpers/generateRandomQuote.js
+++ server/helpers/generateRandomQuote.js
@@ -1,14 +1,24 @@
 import openConnection from '../db/connection.js';
 
 // Generates a random quote from the database (for the main page)
-const generateRandomQuote = async () => {
+const generateRandomQuote = async (category) => {
   let connection;
   try {
     connection = await openConnection();
     await connection.query('USE quotes');
+
+    // If a category is not specified, get a random quote from the database
+    let query = 'SELECT * FROM quotes ORDER BY RAND() LIMIT 1';
+    let params = [];
+
+    // If a category is specified, get a random quote from the specified category
+    if (category) {
+      query = `SELECT * FROM quotes WHERE category = ? ORDER BY RAND() LIMIT 1`;
+      params = [category];
+    }
     // connection.query returns an array of two arrays: the result of a query and the metadata
     // const [randomQuote] is destructuring the first array
-    const [randomQuote] = await connection.query('SELECT * FROM quotes ORDER BY RAND() LIMIT 1');
+    const [randomQuote] = await connection.query(query, params);
 
     return randomQuote[0];
   } catch(error) {
