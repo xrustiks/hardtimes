b6da8ac32c043edad4ed0a48be4ee28d616dceab
xrustik666 <kilorust@gmail.com>
2024-11-21 23:32:23 +0100

    now it looks more like an app

--- client/src/components/Header/headerComponents/SearchField.jsx
+++ client/src/components/Header/headerComponents/SearchField.jsx
@@ -4,8 +4,7 @@ const SearchField = () => {
   return (
     <div className="search-field">
         <form action="/search" method="GET">
-          <input type="search" name="query" placeholder="Введите ключевые слова..." required />
-          <button type="submit">Поиск</button>
+          <input type="search" name="query" placeholder="Поиск цитаты" required />
         </form>
     </div>
   )
