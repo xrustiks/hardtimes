b045216c1d8706f6bb76ce351262fec5af2d99a3
xrustik666 <kilorust@gmail.com>
2024-11-23 00:55:58 +0100

    refactored: RandomQuote is separate component now

--- client/src/components/Main/mainComponents/Home.jsx
+++ client/src/components/Main/mainComponents/Home.jsx
@@ -1,19 +1,11 @@
-import { useState, useEffect, useContext } from 'react';
-import { CategoriesContext } from '../../../hooks/CategoriesContext.jsx';
+import { useEffect } from 'react';
 
 import makeTitle from "../../../utils/makeTitle.js";
-import { addToFavorites } from "../../../utils/favoritesUtils.js";
 import particlesjsConfig from "../../../assets/particlesjs-config.json";
+import RandomQuote from "./RandomQuote.jsx";
 import './Home.css';
 
 const Home = () => {
-  const [chosenCategory] = useContext(CategoriesContext);
-  const [randomQuote, setRandomQuote] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [message, setMessage] = useState("");
-
-  const token = localStorage.getItem('token');
-
   useEffect(() => {
     // Making title for the component
     makeTitle("Главная");
@@ -26,80 +18,12 @@ const Home = () => {
     }
   }, []);
 
-  const fetchRandomQuote = async () => {
-    try {
-      // Fetching a random quote from the server depending on the chosen category
-      const url = chosenCategory
-        ? `http://localhost:3000/api/random-quote?category=${chosenCategory}`
-        : 'http://localhost:3000/api/random-quote';
-
-      const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-      const result = await response.json();
-      setRandomQuote(result);
-    } catch (error) {
-      console.error('Error fetching random quote:', error);
-    }
-  }
-
-  useEffect(() => {
-    // Fetching a random quote when the component is mounted
-    fetchRandomQuote();
-
-    // Updates the quote when the user presses the keyboard button
-    const handleKeyPress = (event) => {
-      if (event.key === "r" || event.key === "к") {
-        fetchRandomQuote();
-      }
-    };
-
-    window.addEventListener("keydown", handleKeyPress);
-
-    return () => {
-      window.removeEventListener("keydown", handleKeyPress);
-    };
-  }, [chosenCategory]);
-
   return (
     <div className="home-page">
       { /* ParticlesJS background */ }
       <div id="particles-js"></div>
-      { /* Component content */ }
-      <div className="random-quote">
-        { randomQuote ? (
-          <blockquote>
-            <div className="quote">&quot;{ randomQuote.quote }&quot;</div>
-            <div className="quote-category">Категория: { randomQuote.category }</div>
-            <div className="quote-origin">Источник: { randomQuote.origin }</div>
-            <footer className="quote-author">Автор: { randomQuote.author }</footer>
-
-            <button
-              className="add-to-favorites-button"
-              type="submit"
-              onClick={ () => addToFavorites(token, randomQuote, setIsLoading, setMessage) }
-              disabled={ isLoading }
-            >
-              { isLoading ? 'Adding...' : 'Add to favorites' }
-            </button>
-            { message && <p>{ message }</p> }
-          </blockquote>
-        ) : (
-          <blockquote>
-            <p>Загрузка...</p>
-          </blockquote>
-        ) }
-
-        <button
-          className="generate-quote-button"
-          type="button"
-          onClick={ () => fetchRandomQuote() }>
-          Next quote
-        </button>
-      </div>
+      { /* RandomQuote component */ }
+      <RandomQuote />
     </div>
   );
 }
