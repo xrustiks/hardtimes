1d3823419774b6163f6dd59f24f8cc0a5adaad7b
xrustik666 <kilorust@gmail.com>
2024-12-06 19:46:05 +0100

    animation works after 'Next quote' button is clicked

--- client/src/components/Main/mainComponents/RandomQuote.jsx
+++ client/src/components/Main/mainComponents/RandomQuote.jsx
@@ -1,6 +1,8 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const RandomQuote = ({ fetchRandomQuote, randomQuote, chosenCategory, message }) => {
+  const [animationKey, setAnimationKey] = useState(0);
+
   useEffect(() => {
     // Fetching a random quote when the component is mounted
     fetchRandomQuote();
@@ -19,18 +21,24 @@ const RandomQuote = ({ fetchRandomQuote, randomQuote, chosenCategory, message })
     };
   }, [chosenCategory]);
 
+  useEffect(() => {
+    // Changing key to reload the animation
+    setAnimationKey((prevKey) => prevKey + 1);
+  }, [randomQuote]);
+
   return (
     <div className="random-quote">
       { randomQuote ? (
         <blockquote>
-          <div className="quote">&quot;{ randomQuote.quote }&quot;</div>
+          { /* When an element is given a new key,
+          React thinks a new element has been created, even if the content has not changed */ }
+          <div className="quote" key={ animationKey }>&quot;{ randomQuote.quote }&quot;</div>
 
           <div className="quote-details">
             <div className="quote-category">Категория: { randomQuote.category }</div>
             <div className="quote-origin">Источник: { randomQuote.origin }</div>
             <footer className="quote-author">Автор: { randomQuote.author }</footer>
           </div>
-          
 
           { message && <p>{ message }</p> }
         </blockquote>
