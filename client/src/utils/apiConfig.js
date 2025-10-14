31eb2b006db191660f3c491c253f819cefa6a426
xrustiks <kilorust@gmail.com>
2025-10-14 14:46:17 +0200

    layout refactoring

--- client/src/utils/apiConfig.js
+++ client/src/utils/apiConfig.js
@@ -1,7 +1,5 @@
 // Uses environment variables to switch between development and production URLs
 const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
-console.log(API_BASE_URL);
-console.log(import.meta.env);
 
 const getApiUrl = (endpoint) => {
   // Removing leading slash if present to avoid double slashes
